cefb4603949f5ea0a80395b865302a752b539ede
yusuf çetin <yusufpau@outlook.com>
2022-11-26 20:51:07 +0300

    tasarim

---
+++ app/src/main/java/com/deliysuf/odevtasarim/MainActivity.java
@@ -0,0 +1,17 @@
+package com.deliysuf.odevtasarim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+}}
